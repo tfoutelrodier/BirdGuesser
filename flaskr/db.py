5b5d5f6ee4313ed8751a0ee347b45f25a24c1d09
tfoutelrodier <theo.foutelrodier@orange.fr>
2025-03-19 23:51:35 +0100

    Added bird data loading into session state upon webapp start

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,8 +1,62 @@
+import json
+import pathlib
+import csv
+import random
+
 import sqlite3
 import click  # deal with command line options for the program
 from flask import current_app, g
 import pandas as pd
-import json
+
+
+def load_random_rows_from_csv(filepath:pathlib.Path, nb_rows=100) -> pd.DataFrame:
+    # avoid loading all the file at once
+    sample = []
+    with open(filepath, mode="r", encoding="utf-8") as file:
+        reader = csv.reader(file, delimiter="|")
+        header = next(reader)  # Read and store the header
+        
+        for i, row in enumerate(reader, start=1):
+            if len(sample) < nb_rows:
+                sample.append(row)  # Fill the reservoir
+            else:
+                r = random.randint(0, i)
+                if r < nb_rows:
+                    sample[r] = row  # Replace an existing row
+
+    df = pd.DataFrame(sample, columns=header)
+    
+    return df
+
+
+def load_bird_names(filepath: pathlib.Path) -> list[str]:
+    # return all bird names (english names) as a list
+    bird_names = []
+    name_index = None
+    with open(filepath, mode="r", encoding="utf-8") as file:
+        reader = csv.reader(file, delimiter="|")
+        header = next(reader)  # Read header
+        
+        for i, col_name in enumerate(header):
+            if col_name == 'en':
+                name_index = i
+        if name_index is not None:
+            for _, row in enumerate(reader, start=1):
+                bird_names.append(row[name_index])
+    
+    return bird_names
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def records2list_of_dict(rows, data_col_lst):
--- flaskr/landing.py
+++ flaskr/landing.py
@@ -4,13 +4,29 @@ Landing page when launching the game
 Blueprint for this page
 Allows to select between all options available (train and play at first)
 """
-from flask import Blueprint, render_template
+import pathlib
+
+from flask import Blueprint, render_template, session
+
+from db import load_random_rows_from_csv, load_bird_names
 
 landing_bp = Blueprint('landing', __name__)
 
 @landing_bp.route('/')
 def index():
     """landing page with default informations"""
+    data_path = pathlib.Path('data/french_bird_data_test.csv')
+
+    # load random birds that will be used when playing on app launch
+    if 'data_df' not in session:
+        # If the sample isn't in session, generate it
+        data_df = load_random_rows_from_csv(data_path, 100)
+        session['data_df'] = data_df.to_json(orient='split')  # Save as JSON
+
+    if 'bird_name_list' not in session:
+        session['bird_name_list'] = load_bird_names(data_path)
+        
+
     return render_template('landing/index.html', 
                           title='BirdGuesser',
                           subtitle='Train to identify birds by their songs')
