0f5b3212dcdba4ee6e6aeed6ec4504fc80b1ed70
tfoutelrodier <theo.foutelrodier@orange.fr>
2025-03-20 01:45:05 +0100

    set session type to filesystem for handling sound data

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,9 @@
 # Main file
 # Run using python BirdGuesser/flaskr/app.py
+import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, current_app
+from flask_session import Session
 
 # Import blueprints
 from landing import landing_bp
@@ -9,6 +11,17 @@ from game import game_bp
 from training import training_bp
 
 app = Flask(__name__)
+
+# Data is stored in file server side because file could take a few Mb with sounds
+app.config['SESSION_TYPE'] = 'filesystem'
+# use os.getcwd rather than current_app because outside of app at the moment
+app.config['SESSION_FILE_DIR'] = os.path.join(os.getcwd(), 'tmp', 'flask_sessions')
+app.config['SESSION_PERMANENT'] = False
+app.secret_key = 'test_key'
+
+
+# Initialize Flask-Session
+Session(app)
 
 # Register blueprints
 app.register_blueprint(landing_bp)
