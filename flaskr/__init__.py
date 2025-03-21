f578ea2f7b11f2f996590f000bc76eddb50ccc9b
tfoutelrodier <theo.foutelrodier@orange.fr>
2025-03-21 12:45:36 +0100

    Added varaible session data folder to accomodate vercel restrictions

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -24,7 +24,13 @@ load_dotenv()  # for loading snesitive data
 
 # Data is stored in file server side because file could take a few Mb with sounds
 app.config['SESSION_TYPE'] = 'filesystem'
-session_folder = os.path.join(os.path.dirname(__file__), 'tmp', 'flask_sessions')
+
+hosting = os.getenv('HOSTING', 0)
+# vercel hosting requires storing in a given location
+if hosting == "vercel":
+    session_folder = os.path('/tmp')
+else:
+    session_folder = os.path.join(os.path.dirname(__file__), 'tmp', 'flask_sessions')
 if not os.path.isdir(session_folder):
     os.makedirs(session_folder)  # Convert Path to string
 app.config['SESSION_FILE_DIR'] = session_folder  # Use os.path for compatibility
